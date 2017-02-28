f44e252b698acc45e3e42bbf2b634f9da1a69260
cut1taru <cutitaru.adryan@gmail.com>
2017-03-01 00:09:18 +0200

    mvc refactor

--- app/src/main/java/com/example/astyletech/lebowebview/Activities/About.java
+++ app/src/main/java/com/example/astyletech/lebowebview/Activities/About.java
@@ -1,4 +1,4 @@
-package com.example.astyletech.lebowebview;
+package com.example.astyletech.lebowebview.Activities;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.example.astyletech.lebowebview.Models.Url;
+import com.example.astyletech.lebowebview.R;
 
 
 public class About extends AppCompatActivity {
--- app/src/main/java/com/example/astyletech/lebowebview/Activities/MainActivity.java
+++ app/src/main/java/com/example/astyletech/lebowebview/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.astyletech.lebowebview;
+package com.example.astyletech.lebowebview.Activities;
 
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
@@ -14,6 +14,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.example.astyletech.lebowebview.Models.Url;
+import com.example.astyletech.lebowebview.R;
 
 
 public class MainActivity extends AppCompatActivity {
