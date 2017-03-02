14687c136684542af9d8e72f876486c2afa1c4c5
cut1taru <cutitaru.adryan@gmail.com>
2017-03-02 13:48:22 +0200

    cleaning the code

--- app/src/main/java/com/example/astyletech/lebowebview/Activities/About.java
+++ app/src/main/java/com/example/astyletech/lebowebview/Activities/About.java
@@ -1,15 +1,11 @@
 package com.example.astyletech.lebowebview.Activities;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,7 +18,7 @@ import com.example.astyletech.lebowebview.R;
 public class About extends AppCompatActivity {
     public Url url = new Url();
     private Toolbar toolbar;
-    private DrawerLayout mDrawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +35,7 @@ public class About extends AppCompatActivity {
         ImageView mPhone = (ImageView) findViewById(R.id.phone);
         ImageView mEmail = (ImageView) findViewById(R.id.email);
         ImageView mFacebook = (ImageView) findViewById(R.id.facebook);
-        TextView mMoto=(TextView) findViewById(R.id.moto);
+        TextView mMoto = (TextView) findViewById(R.id.moto);
         mMoto.setTypeface(null, Typeface.ITALIC);
 
         mPhone.setOnClickListener(new View.OnClickListener() {
@@ -74,14 +70,11 @@ public class About extends AppCompatActivity {
 
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // handle arrow click here
         if (item.getItemId() == android.R.id.home) {
-            finish(); // close this activity and return to preview activity (if there is any)
+            finish();
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
