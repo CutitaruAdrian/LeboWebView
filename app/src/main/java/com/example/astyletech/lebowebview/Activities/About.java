c4060b0c3f87017c64ab01d3c4f9202745531e0f
cut1taru <cutitaru.adryan@gmail.com>
2017-03-02 13:26:28 +0200

    add toolbar in About Activity

--- app/src/main/java/com/example/astyletech/lebowebview/Activities/About.java
+++ app/src/main/java/com/example/astyletech/lebowebview/Activities/About.java
@@ -1,11 +1,19 @@
 package com.example.astyletech.lebowebview.Activities;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.astyletech.lebowebview.Models.Url;
 import com.example.astyletech.lebowebview.R;
@@ -13,14 +21,26 @@ import com.example.astyletech.lebowebview.R;
 
 public class About extends AppCompatActivity {
     public Url url = new Url();
+    private Toolbar toolbar;
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         ImageView mPhone = (ImageView) findViewById(R.id.phone);
         ImageView mEmail = (ImageView) findViewById(R.id.email);
         ImageView mFacebook = (ImageView) findViewById(R.id.facebook);
+        TextView mMoto=(TextView) findViewById(R.id.moto);
+        mMoto.setTypeface(null, Typeface.ITALIC);
 
         mPhone.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,5 +71,18 @@ public class About extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
